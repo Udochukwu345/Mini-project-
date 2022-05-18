4f5d2ecfedd1b16d3e453a1f2fc96a33549653fb
Udochukwu345 <97666194+Udochukwu345@users.noreply.github.com>
2022-05-18 05:02:35 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function myfunction (x){
+x.classList.toggle("change")
+}
+function myfunction() {
+    var x=
+    document.getElementsById("nav-link");
+    if(x.className==="nav-link")
+    x.className += "responsive";
+    else{
+        x.className= "nav-link";
+    }
+}
